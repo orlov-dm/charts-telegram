78f1f409767e0aeac958dbd8b894cea73238f409
Dmitrii Orlov <dm.ed.orlov@gmail.com>
2019-04-06 11:39:50 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssCustomProperties = require('postcss-custom-properties');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { 
+            loader: 'style-loader' 
+          },
+          { 
+            loader: 'css-loader',
+            options:
+            {
+              importLoaders: 1
+            }
+          },
+          { 
+            loader: 'postcss-loader', 
+            options: {
+              config: {
+                path: __dirname + '/postcss.config.js'
+              }
+            },
+            // options: {
+            //   ident: 'postcss',
+            //   plugins: () => [
+            //     postcssPresetEnv(/* pluginOptions */),
+            //     postcssCustomProperties(/* pluginOptions */),                
+            //   ]
+            // } 
+          }
+        ]
+      },      
+//      {
+//        test: /\.svg$/,
+//        loader: 'svg-inline-loader'
+//      }
+    ]
+  },
+  watchOptions: {
+    aggregateTimeout: 300 // The default
+  },
+  devtool: "source-map",
+  /*plugins: [
+  ]*/
+};
